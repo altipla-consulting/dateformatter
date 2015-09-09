d7a99bf250ed50459bbe2bb4bc00c51aeadc2761
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-09-09 16:22:55 +0200

    Run go format after generating symbols files.

--- generator/run.sh
+++ generator/run.sh
@@ -6,3 +6,4 @@ go install github.com/altipla-consulting/i18n-dateformatter/generator
 wget http://www.unicode.org/Public/cldr/27.0.1/core.zip -O /tmp/core.zip
 
 generator -locales en,es,fr,ru
+gofmt -w symbols
