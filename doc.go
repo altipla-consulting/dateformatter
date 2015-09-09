38a5a90ce9e3c3cf72cee801f6121fe462f4eac2
Ernesto Alejo Oltra <ernestokarim@gmail.com>
2015-09-09 16:20:43 +0200

    Update docs.

--- doc.go
+++ doc.go
@@ -1,6 +1,10 @@
-// Package dateformatter helps formatting date in the correct locale. It uses data extract from CLDR v27.0.1.
+// Package dateformatter helps formatting date in the correct locale. It uses
+// data extracted from CLDR v27.0.1.
 // 
-// It has the same formatting options as the time package, excluding formatting of timezones and nanoseconds.
+// It has the same formatting options as the time package, excluding timezones
+// and nanoseconds as they are not needed in a daily basis to format user dates
+// and times.
 // 
 // It has support for the following langs: spanish, english, french and russian.
+// More can be added on demand modifying the generator to create the symbols files.
 package dateformatter
